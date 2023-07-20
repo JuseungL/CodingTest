7f57b57dd6fe136a535a41eb594ea1150a7b752f
JuseungL <juseung0619@gmail.com>
2023-07-20 18:48:43 +0900

    SQL SELECT PART

--- SQL/SELECT.sql
+++ SQL/SELECT.sql
@@ -97,6 +97,22 @@ SELECT ANIMAL_ID, NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY NAME, DATETIME DESC;
 
+--ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품ID, 유저ID, 판매량을 출력하는 SQL문을 작성해주세요. OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요. 결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순, 상품ID까지 같다면 유저 ID를 기준으로 오름차순 정렬해주세요.
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d'), PRODUCT_ID, USER_ID, SALES_AMOUNT
+FROM (
+    SELECT SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+    FROM ONLINE_SALE
+    WHERE MONTH(SALES_DATE) = 3 AND YEAR(SALES_DATE) = 2022
+    
+    UNION ALL
+    
+    SELECT SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+    FROM OFFLINE_SALE
+    WHERE MONTH(SALES_DATE) = 3 AND YEAR(SALES_DATE) = 2022
+) AS combined_sales
+ORDER BY SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC;
+
+
 --동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
